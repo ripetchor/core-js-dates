70c5d59583d5b40def202f29d5a93f9b143fc49c
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-02-26 14:41:13 +0500

    feat: add solution for getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -49,8 +49,11 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  return new Intl.DateTimeFormat('en-US', {
+    weekday: 'long',
+    timeZone: 'UTC',
+  }).format(new Date(date));
 }
 
 /**
